86157c68ed7063360c51a06ebae47f0f2cb55eb8
21bca86 <100268305+21bca86@users.noreply.github.com>
2022-03-11 09:12:55 +0530

    Delete Solution 1.py

--- Solution/Solution 1.py
+++
@@ -1,5 +0,0 @@
-#Write a python script to enter two No. & print the sum of it.
-a=int(input("Enter value for a : "))
-b=int(input("Enter value for b : "))
-c=a+b
-print("Sum of {} and {} = {}".format(a,b,c))
